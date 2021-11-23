20cf738dee32acb7179f633e6869958623b5fc96
Маргарита Рождественская <m.rogdestvenskya@mail.ru>
2021-11-24 00:03:25 +0300

    fix GA id

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "UA-113820431-1",
       },
     },
     "gatsby-plugin-react-helmet",
