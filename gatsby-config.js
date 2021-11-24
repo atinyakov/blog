b65d435cd1202c3e66a1a7618f74c825f7070234
Tinyakov Andrey <akoff16@gmail.com>
2021-11-24 23:06:24 +0300

    part4 wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Tinyakov's",
+    title: "Tinyakov's blog",
   },
   plugins: [
     "gatsby-plugin-image",
